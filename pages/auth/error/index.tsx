da62a652b025f7be2b85341437266d6ba8f43a09
tunogya <tunogya@qq.com>
2023-02-11 22:16:21 +0800

    update error dark mode

--- pages/auth/error/index.tsx
+++ pages/auth/error/index.tsx
@@ -1,15 +1,17 @@
-import {Button, Stack, Text} from "@chakra-ui/react";
+import {Button, Stack, Text, useColorModeValue} from "@chakra-ui/react";
 import {useRouter} from "next/router";
 
 const Error = () => {
   const router = useRouter()
   const {error} = router.query
+  const bg = useColorModeValue('white', '#343541')
+  const fontColor = useColorModeValue('black', '#ECECF1')
 
   return (
-    <Stack h={'100vh'} w={'full'} align={"center"} justify={"center"}>
-      <Text fontSize={'sm'}>Error</Text>
-      <Text fontSize={'sm'}>Oops! {error}</Text>
-      <Button onClick={() => router.back()}>Go back</Button>
+    <Stack h={'100vh'} w={'full'} bg={bg} align={"center"} justify={"center"}>
+      <Text fontSize={'sm'} color={fontColor}>Error</Text>
+      <Text fontSize={'sm'} color={fontColor}>Oops! {error}</Text>
+      <Button color={fontColor} onClick={() => router.back()}>Go back</Button>
     </Stack>
   )
 }
