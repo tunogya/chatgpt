9b5d2ee04326a2a428b40e01fdd1fa025b08c15a
tunogya <tunogya@qq.com>
2023-02-11 23:39:53 +0800

    add salt and hash password #1

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   env: {
     JWT_SECRET: process.env.JWT_SECRET,
     MY_AWS_ACCESS_KEY_ID: process.env.MY_AWS_ACCESS_KEY_ID,
-    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY
+    MY_AWS_SECRET_ACCESS_KEY: process.env.MY_AWS_SECRET_ACCESS_KEY,
+    NEXT_PUBLIC_SALT: process.env.NEXT_PUBLIC_SALT
   }
 }
 
--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -8,6 +8,7 @@ import {useMemo, useState} from "react";
 import {useRouter} from "next/router";
 import {useRecoilState} from "recoil";
 import {jwtAtom} from "@/state";
+import * as crypto from "crypto";
 
 const Login = () => {
   const router = useRouter()
@@ -34,7 +35,8 @@ const Login = () => {
       },
       body: JSON.stringify({
         username,
-        password
+        // add salt to password, and hash it
+        password: crypto.createHash('sha256').update(password + process.env.NEXT_PUBLIC_SALT).digest('base64')
       }),
     })
     if (res.status === 200) {
