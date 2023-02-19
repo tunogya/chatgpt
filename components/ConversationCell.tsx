75675681123ff5c8c7852f3a3d2a55e75bb9e12b
tunogya <tunogya@qq.com>
2023-02-19 14:38:21 +0800

    update

--- components/ConversationCell.tsx
+++ components/ConversationCell.tsx
@@ -21,7 +21,7 @@ const ConversationCell: FC<{message: Message}> = ({message}) => {
     <Stack bg={isAi ? chatBgColor : ''} border={isAi ? '1px solid' : 'none'} borderColor={'rgba(0,0,0,0.1)'} w={'full'}
            py={6} px={4} align={'center'}>
       <HStack maxW={['full', 'container.md']} w={'full'} h={'full'} spacing={6} align={'start'}>
-        <Stack bg={isAi ? '#10A37F' : 'bg5'} minW={'30px'} w={'30px'} h={'30px'} p={1} borderRadius={'2px'}>
+        <Stack bg={isAi ? '#10A37F' : 'bg5'} minW={'30px'} w={'30px'} h={'30px'} p={1} borderRadius={'2px'} userSelect={"none"}>
           <chakra.img src={isAi ? '/openai.svg' : '/icon.svg'}/>
         </Stack>
         <Text color={fontColor} fontSize={'md'} fontWeight={'500'}>{content.parts[0]}</Text>
