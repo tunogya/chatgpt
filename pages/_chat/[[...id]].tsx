b975f53bba310ea4a1eddc3d51bbfea2d435ae18
tunogya <tunogya@qq.com>
2023-03-06 22:24:52 +0800

    update fonts

---
+++ pages/_chat/[[...id]].tsx
@@ -0,0 +1,23 @@
+import {
+  Stack,
+  useMediaQuery,
+} from '@chakra-ui/react';
+import MenuMobile from "@/components/MenuMobile";
+import Menu from "@/components/Menu";
+import Conversation from "@/components/Conversation";
+import {useRouter} from "next/router";
+
+const Chat = () => {
+  const [isMobile] = useMediaQuery('(max-width: 768px)')
+  const router = useRouter()
+  const conversation_id = router.query?.id?.[0] as string
+
+  return (
+    <Stack direction={['column', 'column', 'row']} h={'full'} w={'full'} spacing={0}>
+      {isMobile ? <MenuMobile/> : <Menu/>}
+      <Conversation conversation_id={conversation_id} />
+    </Stack>
+  )
+}
+
+export default Chat
