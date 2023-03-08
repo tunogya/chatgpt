5698ba1601627277b14b0f195a7091a4a20ca8d3
tunogya <tunogya@qq.com>
2023-03-09 00:57:26 +0800

    add edit message

--- components/DialogBoxList/DialogBoxItem.tsx
+++ components/DialogBoxList/DialogBoxItem.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import Edit2Icon from "@/components/SVG/Edit2Icon";
 import OpenAIIcon from "@/components/SVG/OpenAIIcon";
 import LikeIcon from "@/components/SVG/LikeIcon";
@@ -21,6 +21,7 @@ export type Message = {
 
 const DialogBoxItem: FC<Message> = ({...props}) => {
   const username = useSelector((state: any) => state.user.username);
+  const [editMode, setEditMode] = useState(false);
 
   if (props.role === 'user') {
     return (
@@ -35,18 +36,46 @@ const DialogBoxItem: FC<Message> = ({...props}) => {
             </div>
           </div>
           <div className="relative flex w-[calc(100%-50px)] flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]">
-            <div className="flex flex-grow flex-col gap-3">
-              <div className="min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap">
-                {props.content.parts[0]}
-              </div>
-            </div>
-            <div
-              className="text-gray-400 flex self-end lg:self-center justify-center mt-2 gap-3 md:gap-4 lg:gap-1 lg:absolute lg:top-0 lg:translate-x-full lg:right-0 lg:mt-0 lg:pl-2 visible">
-              <button
-                className="p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400 md:invisible md:group-hover:visible">
-                <Edit2Icon/>
-              </button>
-            </div>
+            {
+              editMode ? (
+                <div className="flex flex-grow flex-col gap-3">
+                  <textarea className="m-0 resize-none border-0 bg-transparent p-0 focus:ring-0 focus-visible:ring-0"
+                    style={{height: '24px', overflowY: 'hidden'}}
+                  >
+                    {props.content.parts[0]}
+                  </textarea>
+                  <div className="text-center mt-2 flex justify-center">
+                    <button className="btn relative btn-primary mr-2" onClick={() => {
+                      // TODO: update message
+                    }}>
+                      <div className="flex w-full items-center justify-center gap-2">保存并提交</div>
+                    </button>
+                    <button className="btn relative btn-neutral" onClick={() => {
+                      setEditMode(false);
+                    }}>
+                      <div className="flex w-full items-center justify-center gap-2">取消</div>
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <div className="flex flex-grow flex-col gap-3">
+                    <div className="min-h-[20px] flex flex-col items-start gap-4 whitespace-pre-wrap">
+                      {props.content.parts[0]}
+                    </div>
+                  </div>
+                  <div className="text-gray-400 flex self-end lg:self-center justify-center mt-2 gap-3 md:gap-4 lg:gap-1 lg:absolute lg:top-0 lg:translate-x-full lg:right-0 lg:mt-0 lg:pl-2 visible">
+                    <button className="p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400 md:invisible md:group-hover:visible"
+                            onClick={() => {
+                              setEditMode(true)
+                            }}
+                    >
+                      <Edit2Icon/>
+                    </button>
+                  </div>
+                </>
+              )
+            }
             <div className="flex justify-between"></div>
           </div>
         </div>
