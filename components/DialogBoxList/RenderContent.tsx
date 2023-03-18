0be188cea6f2dc3d03e4af55295dbfc52b0427c5
tunogya <tunogya@qq.com>
2023-03-18 18:09:31 +0800

    support katex

--- components/DialogBoxList/RenderContent.tsx
+++ components/DialogBoxList/RenderContent.tsx
@@ -6,14 +6,28 @@ type RenderContentProps = {
 }
 
 const RenderContent: FC<RenderContentProps> = ({...props}) => {
-  /**
-   * If the content starts with $$ and ends with $$, then it is a math formula
-   * use katex to render the formula
-   */
   if (props.content.startsWith('$$') && props.content.endsWith('$$')) {
     return (
       <p>
         <span dangerouslySetInnerHTML={{__html: katex.renderToString(props.content.slice(2, -2))}}/>
+      </p>
+    )
+  }
+
+  if (props.content.includes('$')) {
+    const parts = props.content.split('$')
+    return (
+      <p>
+        {
+          parts.map((part, index) => {
+            if (index % 2 === 0) {
+              return part
+            }
+            return (
+              <span key={index} dangerouslySetInnerHTML={{__html: katex.renderToString(part)}}/>
+            )
+          })
+        }
       </p>
     )
   }
