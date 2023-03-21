f5b89f96c3446e519be43273bf6ce98c4591fb5a
tunogya <tunogya@qq.com>
2023-03-21 23:02:16 +0800

    update

--- components/DialogMenuList/index.tsx
+++ components/DialogMenuList/index.tsx
@@ -1,26 +1,20 @@
 import DialogMenuItem, {ConversationItemProps} from "./DialogMenuItem";
 import {setConversation} from "@/store/session";
 import {useDispatch, useSelector} from "react-redux";
-import {useCallback, useEffect} from "react";
+import {useEffect} from "react";
 import LoadingIcon from "@/components/SVG/LoadingIcon";
 import useSWR from 'swr'
 
 const DialogMenuList = () => {
   const dispatch = useDispatch();
-  const session = useSelector((state: any) => state.session.session);
   const conversation = useSelector((state: any) => state.session.conversation);
-
   const {data, isLoading} = useSWR('/api/conversation', (url: string) => fetch(url).then((res) => res.json()))
 
-  const getConversationHistory = useCallback(() => async () => {
+  useEffect(() => {
     if (data) {
-      dispatch(setConversation(data.items || []));
+      dispatch(setConversation(data?.items || []));
     }
   }, [data])
-
-  useEffect(() => {
-    getConversationHistory()
-  }, [getConversationHistory, session?.id])
 
   return (
     <div className="flex-col flex-1 overflow-y-auto border-b border-white/20">
