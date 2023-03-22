d4ed56d9b67a76497a75cd43c04fcdd6e6c76efb
tunogya <tunogya@qq.com>
2023-03-22 13:22:19 +0800

    delete redux-persist

--- store/index.ts
+++ store/index.ts
@@ -1,25 +1,14 @@
-import {combineReducers, configureStore} from '@reduxjs/toolkit'
+import {configureStore} from '@reduxjs/toolkit'
 import sessionReducer from './session'
 import uiReducer from './ui'
-import storage from 'redux-persist/lib/storage'
-import { persistReducer } from 'redux-persist'
 import thunk from 'redux-thunk';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['ui'],
-};
-
-const reducers = combineReducers({
-  ui: uiReducer,
-  session: sessionReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    ui: uiReducer,
+    session: sessionReducer,
+  },
   devTools: process.env.NODE_ENV !== 'production',
   middleware: [thunk]
 });
