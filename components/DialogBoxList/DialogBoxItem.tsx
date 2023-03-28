48a0a5b8c5d65ce5c7c5ab459c7c76b5e92331f3
tunogya <tunogya@qq.com>
2023-03-28 17:32:17 +0800

    fixed markdown

--- components/DialogBoxList/DialogBoxItem.tsx
+++ components/DialogBoxList/DialogBoxItem.tsx
@@ -128,7 +128,7 @@ const BaseDialogBoxItem: FC<BaseDialogBoxItemProps> = ({...props}) => {
                   }
                 }}
                 className={`${!!showStreaming ? "result-streaming" : ""} markdown prose w-full break-words dark:prose-invert light`}>
-                {props.message.content.parts[0].trim().replace(/(^\s*)|(\s*$)/g, "")}
+                {props.message.content.parts[0].trim().replace(/[\n\r]+/g, '\n')}
               </ReactMarkdown>
             </div>
           </div>
