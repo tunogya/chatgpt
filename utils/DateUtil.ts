6d8fc1d721671168c24f372a5c6bdd0f297d3d05
tunogya <tunogya@qq.com>
2023-04-10 22:17:41 +0800

    fixed week id

--- utils/DateUtil.ts
+++ utils/DateUtil.ts
@@ -4,5 +4,5 @@ export function getCurrentWeekId(): string {
   const firstDayOfYear: Date = new Date(currentDate.getFullYear(), 0, 1);
   const firstSunday = new Date(year, 0, 1 + (7 - firstDayOfYear.getDay()));
   const weekNumber = Math.ceil(((currentDate.getTime() - firstSunday.getTime()) / 86400000 + 1) / 7);
-  return weekNumber.toString();
+  return `${year}-${weekNumber}`
 }
