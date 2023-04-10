b01674c5a8fe6ccfa9b24188f8e6bbf2ec005b53
tunogya <tunogya@qq.com>
2023-04-11 00:59:30 +0800

    update

--- components/Beian/index.tsx
+++ components/Beian/index.tsx
@@ -2,13 +2,21 @@ import Image from "next/image";
 
 const Beian = () => {
   return (
-    <div className="flex justify-center px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6 space-x-1.5">
-      <a href={"https://beian.miit.gov.cn"}>
-        <Image src={"/images/beian.png"} width={16} height={16} alt={"备案图标"}/>
-      </a>
-      <a href={"https://beian.miit.gov.cn"}>
-        苏ICP备2023010308号-1
-      </a>
+    <div
+      className="flex flex-col items-center px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6 space-y-1">
+      <div className={'flex space-x-1.5'}>
+        <a href={"/doc/PrivacyPolicy"} target={'_blank'} rel={'noreferrer'}>
+          隐私政策
+        </a>
+      </div>
+      <div className={'flex space-x-1.5'}>
+        <a href={"https://beian.miit.gov.cn"}>
+          <Image src={"/images/beian.png"} width={16} height={16} alt={"备案图标"}/>
+        </a>
+        <a href={"https://beian.miit.gov.cn"}>
+          苏ICP备2023010308号-1
+        </a>
+      </div>
     </div>
   )
 }
