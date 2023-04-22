2d4621965771cf637e8ed0d7c99eb6521aaa500b
tunogya <tunogya@qq.com>
2023-04-22 20:30:28 +0800

    fixed WINXIN_APIV3_KEY

--- pages/api/pay/weixin/callback.ts
+++ pages/api/pay/weixin/callback.ts
@@ -5,18 +5,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("收到微信支付的回调请求")
-  console.log(req.headers)
-  console.log(req.body)
-  // Only accept POST requests
-  if (req.method !== 'POST') {
-    res.status(405).json({error: 'Method Not Allowed'});
-    return;
-  }
   // 获取请求的签名
   const signature = req.headers['wechatpay-signature'] as string;
   try {
-    // 验证签名
     const isValid = await pay.verifySign({
       timestamp: req.headers['wechatpay-timestamp'] as string,
       nonce: req.headers['wechatpay-nonce'] as string,
@@ -24,7 +15,6 @@ export default async function handler(
       serial: req.headers['wechatpay-serial'] as string,
       signature,
     })
-    console.log("isValid", isValid)
     if (!isValid) {
       res.status(400).json({error: 'invalid signature'});
       return;
@@ -38,7 +28,6 @@ export default async function handler(
   console.log("resource", resource)
   console.log("准备更新用户的metadata信息")
   try {
-    console.log(req.body);
     res.status(200).json({
       status: 'ok',
     });
