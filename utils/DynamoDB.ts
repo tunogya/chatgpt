228ab5879aa30a72aaf037b4115d0669549e31fc
tunogya <tunogya@qq.com>
2023-05-13 00:09:49 +0800

    update AWS env name

--- utils/DynamoDB.ts
+++ utils/DynamoDB.ts
@@ -4,8 +4,8 @@ import {DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
 const ddbClient = new DynamoDBClient({
   region: 'ap-northeast-1',
   credentials: {
-    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID || '',
-    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY || '',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
   }
 });
 
