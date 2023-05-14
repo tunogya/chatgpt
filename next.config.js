23f4ef8699a43f652e866366c20f974b72bcea00
tunogya <tunogya@qq.com>
2023-05-14 20:55:20 +0800

    fixed env

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     OPENAI_API_SECRET: process.env.OPENAI_API_SECRET,
     WEIXIN_APIV3_KEY: process.env.WEIXIN_APIV3_KEY,
+    AUTH0_ADMIN_CLIENT_ID: process.env.AUTH0_ADMIN_CLIENT_ID,
+    AUTH0_ADMIN_CLIENT_SECRET: process.env.AUTH0_ADMIN_CLIENT_SECRET,
+    ALIYUN_ACCESS_ID: process.env.ALIYUN_ACCESS_ID,
+    ALIYUN_ACCESS_SECRET: process.env.ALIYUN_ACCESS_SECRET,
   },
   images: {
     remotePatterns: [
