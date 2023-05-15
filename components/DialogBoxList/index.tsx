fb9ffaca48aa866dcbb41c5ec3bd0a0185986b9b
tunogya <tunogya@qq.com>
2023-05-15 21:51:13 +0800

    update sort

--- components/DialogBoxList/index.tsx
+++ components/DialogBoxList/index.tsx
@@ -100,35 +100,6 @@ const DialogBoxListContent = () => {
                     <div className="md:flex items-start text-center gap-3.5">
                       <div className="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
                         <h2 className="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
-                          <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24"
-                               strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6" height="1em" width="1em"
-                               xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="12" cy="12" r="5"></circle>
-                            <line x1="12" y1="1" x2="12" y2="3"></line>
-                            <line x1="12" y1="21" x2="12" y2="23"></line>
-                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-                            <line x1="1" y1="12" x2="3" y2="12"></line>
-                            <line x1="21" y1="12" x2="23" y2="12"></line>
-                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-                          </svg>
-                          快速入门
-                        </h2>
-                        <ul className="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
-                          <a href={'https://www.explainthis.io/zh-hans/chatgpt/start'} rel={'noreferrer'} target={'_blank'}
-                             className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">应用和教学 →
-                          </a>
-                          <a href={'https://www.explainthis.io/zh-hans/chatgpt'} rel={'noreferrer'} target={'_blank'}
-                            className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">ChatGPT 指令大全 →
-                          </a>
-                          <a href={'https://promptperfect.jina.ai'} rel={'noreferrer'} target={'_blank'}
-                             className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">优化提示词 Prompt →
-                          </a>
-                        </ul>
-                      </div>
-                      <div className="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
-                        <h2 className="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
                           <LightingIcon/>
                           能力
                         </h2>
@@ -166,6 +137,35 @@ const DialogBoxListContent = () => {
                           <li
                             className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">对 2021 年后的世界和事件的了解有限
                           </li>
+                        </ul>
+                      </div>
+                      <div className="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                        <h2 className="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                          <svg stroke="currentColor" fill="none" strokeWidth="1.5" viewBox="0 0 24 24"
+                               strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6" height="1em" width="1em"
+                               xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="12" cy="12" r="5"></circle>
+                            <line x1="12" y1="1" x2="12" y2="3"></line>
+                            <line x1="12" y1="21" x2="12" y2="23"></line>
+                            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                            <line x1="1" y1="12" x2="3" y2="12"></line>
+                            <line x1="21" y1="12" x2="23" y2="12"></line>
+                            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                          </svg>
+                          快速入门
+                        </h2>
+                        <ul className="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
+                          <a href={'https://www.explainthis.io/zh-hans/chatgpt/start'} rel={'noreferrer'} target={'_blank'}
+                             className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">应用和教学 →
+                          </a>
+                          <a href={'https://www.explainthis.io/zh-hans/chatgpt'} rel={'noreferrer'} target={'_blank'}
+                             className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">ChatGPT 指令大全 →
+                          </a>
+                          <a href={'https://promptperfect.jina.ai'} rel={'noreferrer'} target={'_blank'}
+                             className="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">优化提示词 Prompt →
+                          </a>
                         </ul>
                       </div>
                     </div>
