2b575c74ce7d30592de324cbc4d6c52365158b48
tunogya <tunogya@qq.com>
2023-05-19 15:37:31 +0800

    update login page

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,5 +1,6 @@
 import {useRouter} from "next/router";
 import AbandonIcon from "@/components/SVG/AbandonIcon";
+import Typewriter from "@/components/Typewriter";
 
 const Login = () => {
   const router = useRouter()
@@ -7,8 +8,11 @@ const Login = () => {
   return (
     <div className="w-full h-full flex justify-center items-center flex-col bg-gray-50 dark:bg-gray-800">
       <div className="w-96 flex flex-col justify-center items-center">
-        <div className="mb-5 text-gray-700 dark:text-gray-100 animated hover:intensifies">
-          <AbandonIcon width={'140'}/>
+        <div className="flex flex-col justify-center items-center mb-12 text-gray-700 dark:text-gray-100 animated hover:intensifies">
+          <AbandonIcon width={'200'}/>
+          <div className={'result-streaming'}>
+            <Typewriter text={'ChatGPT'}/>
+          </div>
         </div>
         <div className="mb-4 text-center text-xs dark:text-white">由 OpenAI 提供技术支持</div>
         <div className="flex flex-row gap-3">
