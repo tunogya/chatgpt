ee2b979242e664e25f160d5bc87582c77d35e6e9
tunogya <tunogya@qq.com>
2023-05-22 22:14:57 +0800

    add redis to cache

--- pages/api/moderations/index.ts
+++ pages/api/moderations/index.ts
@@ -1,5 +1,7 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import aliyunClient from "@/utils/Aliyun";
+import redisClient from "@/utils/Redis";
+import * as crypto from "crypto";
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,6 +15,20 @@ export default async function handler(
     res.status(400).json({error: 'Input is required'});
   }
   let flagged = false, blocked = false;
+  const hash = crypto.createHash('sha256').update(input).digest('hex');
+  try {
+    const redisKey = `flag:${hash}`;
+    const redisValue = await redisClient.get(redisKey);
+    if (redisValue) {
+      flagged = JSON.parse(redisValue).flagged;
+      blocked = JSON.parse(redisValue).blocked;
+      console.log("get from redis");
+      res.status(200).json({flagged, blocked});
+      return;
+    }
+  } catch (e) {
+    console.log(e);
+  }
   try {
     const response = await aliyunClient.request('TextModeration', {
       "Service": "ai_art_detection",
@@ -93,6 +109,19 @@ export default async function handler(
     }
   } catch (err) {
     console.log(err);
+  }
+
+  // save result to redis
+  try {
+    const redisKey = `flag:${hash}`;
+    const redisValue = JSON.stringify({
+      flagged,
+      blocked
+    });
+    await redisClient.set(redisKey, redisValue);
+    await redisClient.expire(redisKey, 60 * 60 * 24 * 7); // 7 days
+  } catch (e) {
+    console.log(e);
   }
   res.status(200).json({
     service: 'ai_art_detection',
