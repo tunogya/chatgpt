02922a487fe774d76aa2296e3f03b18334251e36
tunogya <tunogya@qq.com>
2023-05-23 10:50:27 +0800

    fixed redis

--- pages/api/moderations/index.ts
+++ pages/api/moderations/index.ts
@@ -26,7 +26,6 @@ export default withApiAuthRequired(async function handler(
   const hash = crypto.createHash('sha256').update(input).digest('hex');
 
   try {
-    await redisClient.connect();
     const redisKey = `flag:${hash}`;
     const redisValue = await redisClient.get(redisKey);
     if (redisValue) {
@@ -143,7 +142,6 @@ export default withApiAuthRequired(async function handler(
     });
     await redisClient.set(redisKey, redisValue);
     await redisClient.expire(redisKey, 60 * 60 * 24 * 7); // 7 days
-    await redisClient.disconnect();
   } catch (e) {
     console.log(e);
   }
