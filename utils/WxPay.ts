13d1d84ef9e349b5d291c0faf81d290f137b9ba5
tunogya <tunogya@qq.com>
2023-05-26 23:49:13 +0800

    fixed

--- utils/WxPay.ts
+++ utils/WxPay.ts
@@ -4,8 +4,8 @@ import fs from "fs";
 const pay = new WxPay({
   appid: 'wxc7d6f9e23b346d39',
   mchid: '1642508849',
-  publicKey: fs.readFileSync('./apiclient_cert.pem') || process.env.WEIXIN_PUBLIC_KEY,
-  privateKey: fs.readFileSync('./apiclient_key.pem') || process.env.WEIXIN_PRIVATE_KEY,
+  publicKey: fs.readFileSync('./apiclient_cert.pem'),
+  privateKey: fs.readFileSync('./apiclient_key.pem'),
   key: process.env.WEIXIN_APIV3_KEY,
 });
 
