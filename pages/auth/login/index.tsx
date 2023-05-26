b8802f6e0f359aa62ff11657497f0d87b227e890
tunogya <tunogya@qq.com>
2023-05-26 21:06:13 +0800

    auto route to auth0

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,35 +1,14 @@
 import {useRouter} from "next/router";
-import AbandonIcon from "@/components/SVG/AbandonIcon";
-import Typewriter from "@/components/Typewriter";
+import {useEffect} from "react";
 
 const Login = () => {
   const router = useRouter()
 
-  return (
-    <div className="w-full h-full flex justify-center items-center flex-col bg-gray-50 dark:bg-gray-800">
-      <div className="w-96 flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center mb-12 text-gray-700 dark:text-gray-100 animated hover:intensifies">
-          <AbandonIcon width={'200'}/>
-          <div className={'result-streaming'}>
-            <Typewriter text={'ChatGPT'}/>
-          </div>
-        </div>
-        <div className="mb-4 text-center text-xs dark:text-white">由 OpenAI 提供技术支持</div>
-        <div className="flex flex-row gap-3">
-          <button className="btn relative btn-primary" onClick={() => {
-            router.push('/api/auth/login?returnTo=/chat')
-          }}>
-            <div className="flex w-full items-center justify-center gap-2">登陆</div>
-          </button>
-          <button className="btn relative btn-primary" onClick={() => {
-            router.push('/api/auth/login')
-          }}>
-            <div className="flex w-full items-center justify-center gap-2">注册</div>
-          </button>
-        </div>
-      </div>
-    </div>
-  )
+  useEffect(() => {
+    router.push('/api/auth/login')
+  }, [router])
+
+  return <></>
 }
 
 export default Login
