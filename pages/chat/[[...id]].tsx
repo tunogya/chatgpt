9cc383bac93f1ec4c80b2d90fae2a7df0555d780
tunogya <tunogya@qq.com>
2023-05-29 00:44:13 +0800

    update

--- pages/chat/[[...id]].tsx
+++ pages/chat/[[...id]].tsx
@@ -517,25 +517,15 @@ const Chat = ({user}: any) => {
               <div
                 className="flex justify-center px-3 pt-2 pb-3 text-center text-xs text-black/50 dark:text-gray-500 md:px-4 md:pt-3 md:pb-6 space-y-1">
                 <div className={'flex space-x-1.5'}>
-                  {/*<a href={"http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=32068202000378"}*/}
-                  {/*   target={'_blank'} rel={'noreferrer'}>*/}
-                  {/*  /!*<div className={"flex space-x-1.5"}>*!/*/}
-                  {/*    /!*<Image src={"/images/beian.png"} width={16} height={16} alt={"备案图标"}/>*!/*/}
-                  {/*    /!*<p>*!/*/}
-                  {/*    /!*  苏公网安备32068202000378号*!/*/}
-                  {/*    /!*</p>*!/*/}
-                  {/*  /!*</div>*!/*/}
-                  {/*</a>*/}
                   <a href={"https://beian.miit.gov.cn"} target={'_blank'} rel={'noreferrer'}>
                     <div className={"flex space-x-1.5"}>
                       <p>
-                        沪ICP备2023000778号-2
+                        © {new Date().getFullYear() }, Abandon, Inc.
                       </p>
                     </div>
                   </a>
-                  <div>|</div>
                   <div className={"flex space-x-1.5"}>
-                    由 OpenAI 提供技术支持
+                    由 OpenAI 提供技术支持.
                   </div>
                 </div>
               </div>
