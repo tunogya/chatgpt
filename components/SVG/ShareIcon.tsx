5a52893a26dfb62bb0f2de050dc5cde109a6ded8
tunogya <tunogya@qq.com>
2023-06-04 00:04:55 +0800

    update icon

--- components/SVG/ShareIcon.tsx
+++ components/SVG/ShareIcon.tsx
@@ -1,11 +1,10 @@
 const ShareIcon = () => {
   return (
     <svg stroke="currentColor" fill="none" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round"
-         strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em"
-         xmlns="http://www.w3.org/2000/svg">
-      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-      <polyline points="15 3 21 3 21 9"></polyline>
-      <line x1="10" y1="14" x2="21" y2="3"></line>
+         strokeLinejoin="round" className="h-4 w-4" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+      <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"></path>
+      <polyline points="16 6 12 2 8 6"></polyline>
+      <line x1="12" y1="2" x2="12" y2="15"></line>
     </svg>
   )
 }
