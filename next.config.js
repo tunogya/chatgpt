f5bd1fb4ec4b8dcaae97803773ae56ba12d57bdd
tunogya <tunogya@qq.com>
2023-06-27 11:36:08 +0800

    delete unused

--- next.config.js
+++ next.config.js
@@ -11,12 +11,7 @@ const nextConfig = {
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     OPENAI_API_SECRET: process.env.OPENAI_API_SECRET,
     WEIXIN_APIV3_KEY: process.env.WEIXIN_APIV3_KEY,
-    AUTH0_ADMIN_CLIENT_ID: process.env.AUTH0_ADMIN_CLIENT_ID,
-    AUTH0_ADMIN_CLIENT_SECRET: process.env.AUTH0_ADMIN_CLIENT_SECRET,
-    ALIYUN_ACCESS_ID: process.env.ALIYUN_ACCESS_ID,
-    ALIYUN_ACCESS_SECRET: process.env.ALIYUN_ACCESS_SECRET,
     REDIS_URL: process.env.REDIS_URL,
-    NOTION_TOKEN: process.env.NOTION_TOKEN,
   },
   images: {
     remotePatterns: [
