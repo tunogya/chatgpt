ea2520864c00ebd7ad8b281929b0d0015f027c4c
tunogya <tunogya@qq.com>
2023-07-29 22:41:58 +0800

    update

--- pages/chat/[[...id]].tsx
+++ pages/chat/[[...id]].tsx
@@ -490,9 +490,8 @@ const Chat = ({user}: any) => {
                   </div>
                   <div
                     className="flex flex-col w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-xl shadow-xs dark:shadow-xs">
-                <textarea tabIndex={0} data-id="root" style={{maxHeight: 200, height: "24px", overflowY: 'hidden'}}
-                          disabled={!standard_exp}
-                          rows={1} ref={inputRef}
+                <textarea tabIndex={0} data-id="root" style={{maxHeight: 200, height: "24px", overflowY: 'auto'}}
+                          disabled={!standard_exp} rows={1} ref={inputRef}
                           placeholder={standard_exp > 0 ? 'Send a message' : 'Please purchase a membership to continue.'}
                           onKeyDown={async (e) => {
                             if (e.key === 'Enter' && !e.shiftKey) {
@@ -517,7 +516,7 @@ const Chat = ({user}: any) => {
                     {
                       standard_exp > 0 && (
                         <button
-                          className="absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 enabled:bg-brand-green text-white bottom-1.5 transition-colors disabled:opacity-40"
+                          className={`absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 ${session.model === OPENAI_MODELS.GPT4.model ? 'enabled:bg-brand-purple' : 'enabled:bg-brand-green'} text-white bottom-1.5 transition-colors disabled:opacity-40`}
                           disabled={input === '' || isWaitComplete || isBlockComplete}
                           onClick={async (event) => {
                             event.preventDefault();
