f682764300cccd36b707a7db283da8c4f2a4ed55
tunogya <tunogya@qq.com>
2023-07-29 23:34:03 +0800

    update

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -6,14 +6,19 @@
 import React from 'react';
 import {NextPage} from 'next';
 import {useRouter} from 'next/router';
+import AbandonIcon from "@/components/SVG/AbandonIcon";
 
 const Error: NextPage = () => {
   const router = useRouter();
   return (
-    <div className={''}>
-      <h1>404</h1>
-      <h2>Page not found</h2>
-      <button onClick={() => router.push('/')}>Go to home</button>
+    <div className="w-full h-full flex justify-center items-center flex-col bg-gray-50 dark:bg-gray-800 text-gray-700 dark:text-white">
+      <div className="w-96 flex flex-col justify-center items-center">
+        <h1>404</h1>
+        <div className="mt-2 mb-5">
+          <AbandonIcon width={'140'}/>
+        </div>
+        <a className="mb-2 text-center text-sm underline cursor-pointer" onClick={() => router.push('/')}>Go to home</a>
+      </div>
     </div>
   );
 };
