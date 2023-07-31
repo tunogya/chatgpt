78c279d6cbff2a3a1744ee508f43516a24f4d347
tunogya <tunogya@qq.com>
2023-07-31 14:44:05 +0800

    update

--- pages/api/rate-limit/index.ts
+++ pages/api/rate-limit/index.ts
@@ -1,15 +1,8 @@
-/*
-  x-ratelimit-limit-requests	60	The maximum number of requests that are permitted before exhausting the rate limit.
-  x-ratelimit-limit-tokens	150000	The maximum number of tokens that are permitted before exhausting the rate limit.
-  x-ratelimit-remaining-requests	59	The remaining number of requests that are permitted before exhausting the rate limit.
-  x-ratelimit-remaining-tokens	149984	The remaining number of tokens that are permitted before exhausting the rate limit.
-  x-ratelimit-reset-requests	1s	The time until the rate limit (based on requests) resets to its initial state.
-  x-ratelimit-reset-tokens	6m0s	The time until the rate limit (based on tokens) resets to its initial state.
- */
-
 import {getSession, withApiAuthRequired} from "@auth0/nextjs-auth0";
 import {NextApiRequest, NextApiResponse} from "next";
 import {RATE_LIMIT} from "@/const/misc";
+import ddbDocClient from "@/utils/ddbDocClient";
+import {GetCommand, PutCommand} from "@aws-sdk/lib-dynamodb";
 
 export default withApiAuthRequired(async function handler(
   req: NextApiRequest,
@@ -17,33 +10,78 @@ export default withApiAuthRequired(async function handler(
 ) {
   // @ts-ignore
   const {user} = await getSession(req, res);
-  // 访问redis查询用户的rate-limit记录
-  // `${user.sub}:limit:${new Date().toISOString().slice(0, 10)}`
-  // if (!userLimit) {
-  //   // 如果没有，则创建并返回新的记录，TTL为1天
-  //   userLimit = await redisClient.client.set(`${user.sub}:limit:${new Date().toISOString().slice(0, 10)}`, JSON.stringify({
-  //     'gpt-3.5-turbo': {
-  //       requests: Array(12).fill(0),
-  //       tokens: Array(12).fill(0),
-  //     },
-  //     'gpt-4': {
-  //       requests: Array(12).fill(0),
-  //       tokens: Array(12).fill(0),
-  //     },
-  //   }), {
-  //     EX: 86400,
-  //     NX: true,
-  //   })
-  // }
-  // if (!userLimit) {
-  //   return res.status(500).json({
-  //     message: 'Internal Server Error',
-  //   })
-  // }
-  // userLimit = JSON.parse(userLimit)
-  // let rate_limit = {
-  //   ...RATE_LIMIT
-  // }
-  //
-  // return res.status(200).json(rate_limit)
+  const userLimitRes = await ddbDocClient.send(new GetCommand({
+    TableName: 'wizardingpay',
+    Key: {
+      PK: user.sub,
+      SK: `limit:${new Date().toISOString().slice(0, 10)}`,
+    },
+  }))
+  let userLimit = {
+    'gpt-3.5-turbo': {
+      requests: Array(24).fill(0),
+      tokens: Array(24).fill(0),
+    },
+    'gpt-4': {
+      requests: Array(24).fill(0),
+      tokens: Array(24).fill(0),
+    }
+  }
+  if (!userLimitRes.Item) {
+    // 创建用户的使用限制记录
+    await ddbDocClient.send(new PutCommand({
+      TableName: 'wizardingpay',
+      Item: {
+        PK: user.sub,
+        SK: `limit:${new Date().toISOString().slice(0, 10)}`,
+        TTL: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 365,
+        ...userLimit,
+      }
+    }))
+  } else {
+    userLimit = {
+      'gpt-3.5-turbo': userLimitRes.Item['gpt-3.5-turbo'],
+      'gpt-4': userLimitRes.Item['gpt-4'],
+    }
+  }
+  const hour = new Date().getHours()
+  const period_has_pass = Math.floor(hour / RATE_LIMIT['gpt-3.5-turbo']['reset-hours'])
+
+  const total_request_gpt3_5_turbo = userLimit['gpt-3.5-turbo']
+    .requests
+    .slice(hour - RATE_LIMIT['gpt-3.5-turbo']['reset-hours'] + 1, hour + 1)
+    .reduce((a, b) => a + b, 0)
+  const total_tokens_gpt3_5_turbo = userLimit['gpt-3.5-turbo']
+    .tokens
+    .slice(hour - RATE_LIMIT['gpt-3.5-turbo']['reset-hours'] + 1, hour + 1)
+    .reduce((a, b) => a + b, 0)
+  const total_request_gpt4 = userLimit['gpt-4']
+    .requests
+    .slice(hour - RATE_LIMIT['gpt-4']['reset-hours'] + 1, hour + 1)
+    .reduce((a, b) => a + b, 0)
+  const total_tokens_gpt4 = userLimit['gpt-4']
+    .tokens
+    .slice(hour - RATE_LIMIT['gpt-4']['reset-hours'] + 1, hour + 1)
+    .reduce((a, b) => a + b, 0)
+  const next_start_time_gpt3_5_turbo = new Date(new Date().setHours((period_has_pass + 1) * RATE_LIMIT['gpt-3.5-turbo']['reset-hours'], 0, 0, 0)).getTime()
+  const next_start_time_gpt4 = new Date(new Date().setHours((period_has_pass + 1) * RATE_LIMIT['gpt-4']['reset-hours'], 0, 0, 0)).getTime()
+
+  res.status(200).json({
+    'gpt-3.5-turbo': {
+      'limit-requests': RATE_LIMIT["gpt-3.5-turbo"]["limit-tokens"],
+      'limit-tokens': RATE_LIMIT["gpt-3.5-turbo"]["limit-tokens"],
+      'remaining-requests': RATE_LIMIT['gpt-3.5-turbo']['limit-requests'] - total_request_gpt3_5_turbo,
+      'remaining-tokens': RATE_LIMIT['gpt-3.5-turbo']['limit-tokens'] - total_tokens_gpt3_5_turbo,
+      'reset-requests': (next_start_time_gpt3_5_turbo - new Date().getTime()) / 1000 / 60,
+      'reset-tokens': (next_start_time_gpt3_5_turbo - new Date().getTime()) / 1000 / 60,
+    },
+    'gpt-4': {
+      'limit-requests': RATE_LIMIT["gpt-4"]["limit-requests"],
+      'limit-tokens': RATE_LIMIT["gpt-4"]["limit-tokens"],
+      'remaining-requests': RATE_LIMIT['gpt-4']['limit-requests'] - total_request_gpt4,
+      'remaining-tokens': RATE_LIMIT['gpt-4']['limit-tokens'] - total_tokens_gpt4,
+      'reset-requests': (next_start_time_gpt4 - new Date().getTime()) / 1000 / 60,
+      'reset-tokens': (next_start_time_gpt4 - new Date().getTime()) / 1000 / 60,
+    },
+  })
 })
