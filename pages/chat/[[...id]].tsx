2319e9ae2e7cd0a55bc4e9a96c31bf759f4e49a8
tunogya <tunogya@qq.com>
2023-07-31 22:21:46 +0800

    update

--- pages/chat/[[...id]].tsx
+++ pages/chat/[[...id]].tsx
@@ -82,6 +82,14 @@ const Chat = ({user}: any) => {
     data: rateLimitData,
   } = useSWR('/api/rate-limit', (url: string) => fetch(url).then((res) => res.json()))
 
+  const isRateLimitValid = useMemo(() => {
+    if (session.model === OPENAI_MODELS.GPT3_5.model) {
+      return rateLimitData?.['gpt-3.5-turbo']?.['remaining-requests'] > 0 && rateLimitData?.['gpt-3.5-turbo']?.['remaining-tokens'] > 0
+    } else if (session.model === OPENAI_MODELS.GPT4.model) {
+      return rateLimitData?.['gpt-4']?.['remaining-requests'] > 0 && rateLimitData?.['gpt-4']?.['remaining-tokens'] > 0
+    }
+  }, [rateLimitData, session.model])
+
   const handleSubmit = async () => {
     if (input === '' || isWaitComplete || isBlockComplete) return;
     const scroll_to_bottom_button = document.getElementById('scroll-to-bottom-button');
@@ -500,7 +508,7 @@ const Chat = ({user}: any) => {
                   <div
                     className="flex flex-col w-full py-[10px] flex-grow md:py-4 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-xl shadow-xs dark:shadow-xs">
                 <textarea tabIndex={0} data-id="root" style={{maxHeight: 200, height: "24px", overflowY: 'auto'}}
-                          disabled={!standard_exp} rows={1} ref={inputRef}
+                          disabled={!standard_exp || !isRateLimitValid} rows={1} ref={inputRef}
                           placeholder={standard_exp > 0 ? 'Send a message' : 'Please purchase a membership to continue.'}
                           onKeyDown={async (e) => {
                             if (e.key === 'Enter' && !e.shiftKey) {
@@ -526,7 +534,7 @@ const Chat = ({user}: any) => {
                       standard_exp > 0 && (
                         <button
                           className={`absolute p-1 rounded-md md:bottom-3 md:p-2 md:right-3 dark:hover:bg-gray-900 dark:disabled:hover:bg-transparent right-2 disabled:text-gray-400 ${session.model === OPENAI_MODELS.GPT4.model ? 'enabled:bg-brand-purple' : 'enabled:bg-brand-green'} text-white bottom-1.5 transition-colors disabled:opacity-40`}
-                          disabled={input === '' || isWaitComplete || isBlockComplete}
+                          disabled={input === '' || isWaitComplete || isBlockComplete || !isRateLimitValid}
                           onClick={async (event) => {
                             event.preventDefault();
                             await handleSubmit()
