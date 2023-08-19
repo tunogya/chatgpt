3415d7115ef00761b00a512e6b7cb7986c36cf12
tunogya <tunogya@qq.com>
2023-08-19 22:37:35 +0800

    update

--- pages/api/checkout/callback.ts
+++ pages/api/checkout/callback.ts
@@ -1,46 +1,72 @@
 import auth0Management from "@/utils/auth0Management";
+import {NextApiRequest, NextApiResponse} from "next";
+import Stripe from 'stripe';
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+    apiVersion: '2022-11-15',
+  });
+  const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!;
 
-export default async function handler(req: any, res: any) {
   if (req.method === 'POST') {
-    // get headers stripe-signature
-    let event;
+    const sig = req.headers['stripe-signature']!;
+    let event: Stripe.Event;
+
     try {
-      event = stripe.webhooks.constructEvent(req.body, req.headers['stripe-signature'], process.env.STRIPE_WEBHOOK_SECRET);
-      console.log(event)
+      const body = await buffer(req);
+      event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
     } catch (err: any) {
-      console.log(err.message)
+      // On error, log and return the error message
+      console.log(`❌ Error message: ${err.message}`);
       res.status(400).send(`Webhook Error: ${err.message}`);
       return;
     }
 
-    switch (event.type) {
-      case 'checkout.session.completed':
-        const checkoutSessionCompleted = event.data.object;
-        const { metadata, payment_status} =  checkoutSessionCompleted;
-        console.log(metadata,  payment_status)
-        // payment_status - paid, unpaid, refunded
-        if (payment_status === 'paid') {
-          if (metadata) {
-            try {
-              const auth0Metadata = JSON.parse(metadata.metadata);
-              await auth0Management.updateAppMetadata({id: metadata.id}, {
-                ...auth0Metadata,
-              })
-              console.log('metadata updated', metadata)
-            } catch (e) {
-              console.log(e)
-            }
+    if (event.type === 'checkout.session.completed') {
+      const checkoutSessionCompleted = event.data.object as Stripe.Checkout.Session;
+      const {metadata, payment_status} = checkoutSessionCompleted;
+      if (payment_status === 'paid') {
+        if (metadata) {
+          try {
+            const auth0Metadata = JSON.parse(metadata.metadata);
+            await auth0Management.updateAppMetadata({id: metadata.id}, {
+              ...auth0Metadata,
+            })
+            console.log('metadata updated', metadata)
+          } catch (e) {
+            console.log(e)
           }
         }
-        break;
-      default:
-        break;
+      } else {
+        console.log("no paid!")
+      }
     }
-    res.status(200).json({ received: true });
+
+    res.status(200).json({received: true});
   } else {
     res.setHeader('Allow', 'POST');
     res.status(405).end('Method Not Allowed');
   }
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const buffer = (req: NextApiRequest) => {
+  return new Promise<Buffer>((resolve, reject) => {
+    const chunks: Buffer[] = [];
+
+    req.on('data', (chunk: Buffer) => {
+      chunks.push(chunk);
+    });
+
+    req.on('end', () => {
+      resolve(Buffer.concat(chunks));
+    });
+
+    req.on('error', reject);
+  });
+};
