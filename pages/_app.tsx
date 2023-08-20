a6e5a52ca876bef1db0b67853bff84461c7fbd44
tunogya <tunogya@qq.com>
2023-08-21 04:34:20 +0800

    add kwikembed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -73,6 +73,8 @@ export default function App({Component, pageProps}: AppProps) {
           <link rel='icon' href={'/favicon.png'} type='image/png'/>
           <link rel='apple-touch-icon' href={'/apple-touch-icon.png'} sizes={'180x180'} type='image/png'/>
           <link rel='mask-icon' href={'favicon.svg'} color='#343541' type='image/svg+xml'/>
+          <Script type="module" crossOrigin={"anonymous"} async src={"https://www.kwikembed.com/socialproof.js"} id="kwiksocialproof"
+                  data-account="acct_1MagF9FPpv8QfieY"></Script>
         </Head>
         <Script src={'https://www.googletagmanager.com/gtag/js?id=G-EDPQ3K7EN8'}/>
         <Script id='google-tag-manager' strategy='afterInteractive'>
