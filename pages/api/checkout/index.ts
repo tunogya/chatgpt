77503a11fd6494eae6987799326db56f450f3bc1
tunogya <tunogya@qq.com>
2023-08-20 11:35:56 +0800

    update

--- pages/api/checkout/index.ts
+++ pages/api/checkout/index.ts
@@ -1,6 +1,9 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import Stripe from "stripe";
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2022-11-15',
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
